83d00f9aa3f35fb3284d980f0056ad554e7922d0
German Viescas <germanviescas@gmail.com>
2022-07-18 16:44:31 -0300

    Improve readme upate script

--- scripts/update-readme.js
+++ scripts/update-readme.js
@@ -3,48 +3,34 @@ const fs = require('fs');
 const readline = require('readline');
 const { pointErrorCodes } = require('../dist/index');
 
-const readmePath = path.resolve(__dirname, '..', 'README.md');
-let updatedReadme = '';
-
-function replaceContents() {
-  fs.writeFileSync(readmePath, updatedReadme);
-  console.log('Done updating README');
-}
-
-function generateTable() {
-  updatedReadme += '| code | name | text |\n';
-  updatedReadme += '| ---- | ---- | ---- |\n';
-
-  for (let k in pointErrorCodes) {
-    const e = pointErrorCodes[k];
-    updatedReadme += `| ${e.code} | ${e.name} | ${e.text} |\n`;
-  }
-
-  replaceContents();
-}
-
-function updateReadme() {
-  const rl = readline.createInterface({
-    input: fs.createReadStream(readmePath),
-  });
-
+(function updateReadme() {
+  const readmePath = path.resolve(__dirname, '..', 'README.md');
+  const updatedReadmePath = path.resolve(__dirname, '..', 'README.md.tmp');
+  const writer = fs.createWriteStream(updatedReadmePath);
+  const reader = readline.createInterface({ input: fs.createReadStream(readmePath) });
   let stopCopying = false;
 
-  rl.on('line', line => {
+  // Copy all the README up to the '## Error Codes' line (included).
+  reader.on('line', line => {
     if (!stopCopying) {
-      updatedReadme += line;
-      updatedReadme += '\n';
+      writer.write(`${line}\n`);
     }
 
     if (line === '## Error Codes') {
       stopCopying = true;
-      rl.close();
+      reader.close();
     }
   });
 
-  rl.on('close', () => {
-    generateTable();
-  });
-}
+  // Write the errors table.
+  reader.on('close', () => {
+    writer.write('| code | name | text |\n| ---- | ---- | ---- |\n');
 
-updateReadme();
+    for (let k in pointErrorCodes) {
+      const e = pointErrorCodes[k];
+      writer.write(`| ${e.code} | ${e.name} | ${e.text} |\n`);
+    }
+
+    fs.renameSync(updatedReadmePath, readmePath);
+  });
+})();
